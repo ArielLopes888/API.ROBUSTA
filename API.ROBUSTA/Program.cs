cc77ed5350c996e62850b3839255fe77d043bdae
Ariel Lopes <ariellopes888@gmail.com>
2022-11-17 10:11:42 -0300

    Adcionando e configurando Swagger + outras exclusões de arquivos desnecessários

--- API.ROBUSTA/Program.cs
+++ API.ROBUSTA/Program.cs
@@ -1,18 +1,45 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "AL2",
+        Title = "API ROBUSTA",
+        Description = "Aprendendo a construir uma API robusta e moderna utilizando .NET. Curso do Lucas Eschechola.",
+
+        //License = new OpenApiLicense
+        //{
+        //    Name = "Licença",
+        //    Url = new Uri("https://example.com/license")
+        //}
+    });
+    //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //config.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -20,6 +47,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
